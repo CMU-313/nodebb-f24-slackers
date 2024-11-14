f7a08b3d0c83da4dc31113dd419a8d68105123a6
Phillip Araujo <phillip@lunenetworks.com>
2024-11-14 12:15:56 -0500

    Fixed lint errors, changed testing scope of two linters

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,10 +1,10 @@
-var request = require('request');
+'use strict';
 
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    const TRANSLATOR_API = "https://slackers-translator-d7bcacgqd5a2gsap.canadacentral-01.azurewebsites.net/";
-    const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-    const data = await response.json();
-    return [data["is_english"], data["translated_content"]];
-}
+	const TRANSLATOR_API = 'https://slackers-translator-d7bcacgqd5a2gsap.canadacentral-01.azurewebsites.net/';
+	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
+	const data = await response.json();
+	return [data.is_english, data.translated_content];
+};
